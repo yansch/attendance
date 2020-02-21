24f9632550a9111e7a4013db8d5b78ca192ed238
Jan <j-schnorrenberg@web.de>
2020-02-21 11:34:13 +0100

    Add request interceptor and errors.js

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,6 +2,7 @@
 
 import Vue from 'vue';
 import axios from 'axios';
+import store from './store';
 
 // Full config:  https://github.com/axios/axios#request-config
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
@@ -18,7 +19,10 @@ const _axios = axios.create(config);
 
 _axios.interceptors.request.use(
     function (config) {
-        const token = store.getters.token;
+        if (store.getters.isLoggedIn) {
+            const token = store.getters.token;
+            config.headers.Authorization = `Bearer ${token}`;
+        }
         return config;
     },
     function (error) {
---
+++ src/services/errors.js
@@ -0,0 +1,9 @@
+export default {
+    invalid_email: 'Email-Adresse ungültig',
+    invalid_password: 'Passwort muss zwischen 8 und 32 Zeichen lang sein und darf Buchstaben und Zahlen enthalten',
+    invalid_name: 'Name muss zwischen 3 und 30 Zeichen lang sein und darf nur Buchstaben enthalten',
+    already_exists: 'Email-Adresse wird bereits verwendet',
+    invalid_login: 'Email-Adresse oder Passwort falsch',
+    internal_error: 'Ein Fehler ist aufgetreten',
+    passwords_not_matching: 'Passwörter stimmen nicht überein'
+}
