24e1cd49e3d27c21621e23c81963c38da7694af9
Jan <j-schnorrenberg@web.de>
2020-02-21 10:42:10 +0100

    Add Vuetify and Axios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+    router,
+    store,
+    vuetify,
+
+    render: function (h) {
+        return h(App)
+    }
+}).$mount('#app');
